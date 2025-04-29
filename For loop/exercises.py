71603ebadfd45595455197fbd17e316b6353f996
MorrisGcode <morrisgithinji07@gmail.com>
2025-04-29 21:03:45 +0300

    more loop exercises

---
+++ For loop/exercises.py
@@ -0,0 +1,8 @@
+#Program using For Loop to print numbers 1 to 10 using a range function. 
+
+for i in range(1, 11, 1):
+  print (i)
+# Python Program using the For Loop to print the multiplication table of 5
+
+for num in range(1, 11):
+  print(f"5 * {num} = {5 *num}")
